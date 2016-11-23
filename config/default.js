06fd0aabbcbac48b0fd40b5928a68227af3a9c7c
waruna <waruna@duosoftware.com>
2016-11-23 11:06:28 +0530

    fix case issue

--- config/default.js
+++ config/default.js
@@ -4,7 +4,7 @@ module.exports = {
         "User": "duouser",
         "Password": "DuoS123",
         "Port": 5432,
-        "Host": "192.168.0.39",
+        "Host": "localhost",
         "Database": "dvpdb"
     },
     "Redis": {
